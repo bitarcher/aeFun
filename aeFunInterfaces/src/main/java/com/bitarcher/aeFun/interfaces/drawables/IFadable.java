f3572ec9391e7dca8c8d5ce917f3bbed598ebdc8
Michel Strasser <mstrasser@bitarcher.com>
2015-05-15 16:47:07 +0200

    instruction, vowels presentation

---
+++ aeFunInterfaces/src/main/java/com/bitarcher/aeFun/interfaces/drawables/IFadable.java
@@ -0,0 +1,15 @@
+package com.bitarcher.aeFun.interfaces.drawables;
+
+/*
+ * Copyright (c) 2015.
+ * Michel Strasser
+ * bitarcher.com
+ */
+
+/**
+ * Created by michel on 15/05/15.
+ */
+public interface IFadable {
+    void fadeIn(float numOfSeconds);
+    void fadeOut(float numOfSeconds);
+}
